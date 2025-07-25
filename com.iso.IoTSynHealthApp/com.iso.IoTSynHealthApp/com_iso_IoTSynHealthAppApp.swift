ef2799bbe2e64d35e4482d1256c1d27ff4804bbf
Hai Ho <hai.ho@ptvltd.com>
2025-07-25 16:21:10 +0700

    init code

---
+++ com.iso.IoTSynHealthApp/com.iso.IoTSynHealthApp/com_iso_IoTSynHealthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  com_iso_IoTSynHealthAppApp.swift
+//  com.iso.IoTSynHealthApp
+//
+//  Created by PTV on 25/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct com_iso_IoTSynHealthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
